777deafdf50b6fac446513c815e2bbdbb4acd914
Andre Soares <andregsilv@gmail.com>
2020-11-18 18:22:09 -0300

    remove partial test

--- test/layer3/commands/get_info_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bc108/src/layer3/commands/get_info.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('mapper', () {
-    final sut = Mapper();
-    sut.map\
-  });
-}
