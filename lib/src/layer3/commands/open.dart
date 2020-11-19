f2b04238261a00a87ee52b669529e682e46fbf72
Andre Soares <andregsilv@gmail.com>
2020-11-19 19:07:09 -0300

    fix

--- lib/src/layer3/commands/open.dart
+++ lib/src/layer3/commands/open.dart
@@ -1,20 +1,7 @@
 import 'package:bc108/src/layer2/exports.dart';
 
-import '../fields/numeric.dart';
-import '../fields/alphanumeric.dart';
-import '../fields/composite.dart';
 import '../handler.dart';
 import '../mapper.dart';
-
-class GetInfo00Response {
-  String manufacturer;
-  String modelAndHardwareVersion;
-  bool supportsContactless;
-  String softwareVersion;
-  String specificationVersion;
-  String baseApplicationVersion;
-  String serialNumber;
-}
 
 class Mapper extends RequestResponseMapper<void, void> {
   @override
