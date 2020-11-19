c0fd15041615d5e015f3ee4afb5999d14ddbb02d
Andre Soares <andregsilv@gmail.com>
2020-11-19 18:18:48 -0300

    update lib

--- lib/src/layer1/exports.dart
+++ lib/src/layer1/exports.dart
@@ -3,3 +3,5 @@ export 'read/frame_receiver.dart';
 export 'read/frame_receiver_with_retry.dart';
 export 'write/frame_sender.dart';
 export 'write/frame_builder.dart';
+
+export 'utils/bytes.dart' show IntExtensions;
