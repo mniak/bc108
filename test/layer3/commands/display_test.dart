e4549593f04a124ff6f35eba3aad8885236375a6
Andre Soares <andregsilv@gmail.com>
2020-12-07 17:33:07 -0300

    test display

--- test/layer3/commands/display_test.dart
+++ test/layer3/commands/display_test.dart
@@ -1,4 +1,4 @@
-import 'package:bc108/src/layer3/commands/close.dart';
+import 'package:bc108/src/layer3/commands/display.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,20 +6,20 @@ void main() {
   test('constructor', () {
     final line1 = faker.lorem.sentence();
     final line2 = faker.lorem.sentence();
-    final request = CloseRequest(line1, line2);
+    final request = DisplayRequest(line1, line2);
 
-    expect(request.idleMessageLine1, equals(line1));
-    expect(request.idleMessageLine2, equals(line2));
+    expect(request.line1, equals(line1));
+    expect(request.line2, equals(line2));
   });
 
   test('serialize request padding right with white space', () {
     final sut = Mapper();
 
-    final request = CloseRequest("VICTORIA", "SUPERMARKET");
+    final request = DisplayRequest("VICTORIA", "SUPERMARKET");
 
     final command = sut.mapRequest(request);
 
-    expect(command.code, equals("CLO"));
+    expect(command.code, equals("DSP"));
     expect(command.parameters, hasLength(1));
 
     expect(command.parameters.elementAt(0),
@@ -29,11 +29,11 @@ void main() {
   test('serialize request with very long text', () {
     final sut = Mapper();
 
-    final request = CloseRequest('Vic' * 20, "Market");
+    final request = DisplayRequest('Vic' * 20, "Market");
 
     final command = sut.mapRequest(request);
 
-    expect(command.code, equals("CLO"));
+    expect(command.code, equals("DSP"));
     expect(command.parameters, hasLength(1));
 
     expect(command.parameters.elementAt(0),
