aeb34a7d6a40f97fa5c42e9e77b8ad72032e7fd9
Andre Soares <andregsilv@gmail.com>
2020-12-18 11:40:40 -0300

    delete aborted exception

--- lib/src/layer1/read/exceptions.dart
+++ lib/src/layer1/read/exceptions.dart
@@ -35,16 +35,6 @@ class ExpectingDataEventException implements FrameReceiverException {
 
 abstract class ReaderException implements Exception {}
 
-class AbortedException implements ReaderException {
-  String message;
-  AbortedException() {
-    this.message = "Aborted because the byte EOT (0x04) was received";
-  }
-  String toString() {
-    return "AbortedException: $message";
-  }
-}
-
 class ByteOutOfRangeException implements ReaderException {
   String message;
   ByteOutOfRangeException(int byte) {
